a97f8eaf5c84bdd6dbdba716a835cf613818e38e
✨Futura Veinsss✨ <47320119+veinsss@users.noreply.github.com>
2024-12-06 13:17:25 +0800

    Simplified

--- BeanCoin.sol
+++ BeanCoin.sol
@@ -5,68 +5,19 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BeanCoin is ERC20, Ownable {
-    constructor() ERC20("BeanCoin", "BC") Ownable(msg.sender) {}
+    constructor(uint256 initialSupply) ERC20("BeanCoin", "BC") Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply);
+    }
 
-    event Purchase(address indexed buyer, string item, uint256 amount);
-
-    uint256 public constant americano = 5;
-    uint256 public constant latte = 10;
-    uint256 public constant mocha = 15;
-    uint256 public constant cappuccino = 20;
-
-    function Toast(address to, uint256 amount) external onlyOwner {
-        require(amount > 0, "You can't toast 0 beans");
+    function mint(address to, uint256 amount) external onlyOwner {
+        require(to != address(0), "Cannot mint to zero address");
+        require(amount > 0, "Mint amount must be greater than 0");
         _mint(to, amount);
     }
 
-    function Brew(uint256 amount) external {
+    function burn(uint256 amount) external {
         require(amount > 0, "Burn amount must be greater than 0");
-        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
-
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance to burn");
         _burn(msg.sender, amount);
-    }
-
-    function transferBeans(address recipient, uint256 amount) external {
-        require(recipient != address(0), "Address is Invalid");
-            require(amount > 0, "You can't transfer 0 beans");
-        require(balanceOf(msg.sender) >= amount, "Not enough beans");
-
-        _transfer(_msgSender(), recipient, amount);
-    }
-
-    function buyItem(string memory item) external {
-        uint256 price;
-
-        if (
-            keccak256(abi.encodePacked(item)) ==
-            keccak256(abi.encodePacked("americano"))
-        ) {
-            price = americano;
-        } else if (
-            keccak256(abi.encodePacked(item)) ==
-            keccak256(abi.encodePacked("latte"))
-        ) {
-            price = latte;
-        } else if (
-            keccak256(abi.encodePacked(item)) ==
-            keccak256(abi.encodePacked("mocha"))
-        ) {
-            price = mocha;
-        } else if (
-            keccak256(abi.encodePacked(item)) ==
-            keccak256(abi.encodePacked("cappuccino"))
-        ) {
-            price = cappuccino;
-        } else {
-            revert("Item not available");
-        }
-
-        require(
-            balanceOf(msg.sender) >= price,
-            "Insufficient beans to brew this item"
-        );
-
-        _burn(msg.sender, price);
-        emit Purchase(msg.sender, item, price);
     }
 }
